9d0cc77486dd05ba565752aecf86eebfbd0bbf7a
pchristianoliver <pchristianoliver@gmail.com>
2022-03-03 14:57:17 +0800

    Updated Controllers

---
+++ ContactTracing_API/Controllers/ActivityLogController.cs
@@ -0,0 +1,18 @@
+﻿using ContactTracing_API.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactTracing_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityLogController : ControllerBase
+    {
+        public ActionResult AddActivityLog(ActivityLogModel activityLog) {  
+
+        
+        }
+
+
+    }
+}
